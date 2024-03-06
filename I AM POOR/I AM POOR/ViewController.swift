269c402405422de4fee48b464118d92475cb76d8
Raghavendra Mirajkar <raghavendramirajkar@Raghavendras-MacBook-Air.local>
2024-03-06 17:13:22 +0530

    created the file

---
+++ I AM POOR/I AM POOR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I AM POOR
+//
+//  Created by Raghavendra Mirajkar on 06/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
